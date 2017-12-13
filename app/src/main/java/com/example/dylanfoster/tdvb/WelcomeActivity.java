efd9da4db6f61de6b591c3a02da3fc801eb314bb
dydyFos <dylanfoster.esgi@gmail.com>
2017-12-13 23:03:29 +0100

    switch activity on click 101

--- app/src/main/java/com/example/dylanfoster/tdvb/WelcomeActivity.java
+++ app/src/main/java/com/example/dylanfoster/tdvb/WelcomeActivity.java
@@ -1,13 +1,24 @@
 package com.example.dylanfoster.tdvb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class WelcomeActivity extends AppCompatActivity {
+    public static final String MSG_TO_YOU_RUDY = "STOP YOUR MESSING AROUND";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void btnHandler(View view) {
+        Intent intent = new Intent(this, FullscreenActivity.class);
+        intent.putExtra(MSG_TO_YOU_RUDY, getString(R.string.app_welcome_msg));
+        startActivity(intent);
     }
 }
